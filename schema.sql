7c4a9d20ab9ba2a7510620aa2c0ea593798ddc6f
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-05-21 22:52:58 +0300

    feat(sqlAlchemy): set up sqlAlchemy

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  role TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS USER (
+  RECORD_ID varchar(32),
+  USERNAME varchar(32) unique not null,
+  EMAIL varchar(64) unique not null,
+  FIRSTNAME varchar(64),
+  LASTNAME varchar(64),
+  PASSWORD varchar(128),
+  STATUS varchar(16),
+  primary key(id)
 );
