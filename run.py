21c1da522de23dc6c3bbfbfea9ecd17769582724
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-05-24 22:54:53 +0300

    feat(logging): logging basic format added

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import logging
+
 from app import create_app
+
+__LOGGING_FORMAT = '%(asctime)s [%(levelname)s:] %(message)s'
+__LOGGING_DATEFMT = '%m/%d/%Y %I:%M:%S'
+
+logging.basicConfig(level=logging.NOTSET, 
+                    format=__LOGGING_FORMAT, datefmt=__LOGGING_DATEFMT)
 
 app = create_app()
 app.run()
