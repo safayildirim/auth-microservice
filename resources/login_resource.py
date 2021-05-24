9abbc126c3a178aff4c09463b7a0d8b0c4ba9901
s00599408 <safa.yildirim1@huawei.com>
2021-05-24 08:41:31 +0300

    feat(login): parser implemented.

--- resources/login_resource.py
+++ resources/login_resource.py
@@ -1,21 +1,25 @@
-from flask import request
 from flask_restful import Resource
 
 from models.user import User
+from register_resource import base_parser
 from services import jwt_util
+
+login_parser = base_parser.copy()
+login_parser.remove_argument('username')
 
 
 class LoginResource(Resource):
     def post(self):
         try:
-            user = User.query.filter_by(email=request.json.get('email')).first()
+            args = login_parser.parse_args()
+            user = User.query.filter_by(email=args.email).first()
             if user is None:
                 response = {
                     'status': '401',
                     'message': 'User is not found.'
                 }
                 return response, 401
-            if user.check_password(request.json.get('password')):
+            if user.check_password(args.password):
                 auth_token = jwt_util.encode_auth_token(user.id)
                 if auth_token:
                     response = {
