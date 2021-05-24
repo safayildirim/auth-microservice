a09d482a007d30db8cbc300a08e05ef0dda1b506
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-05-24 22:58:58 +0300

    refactor(login): after adding base and changing error, login refactored to new structure

--- resources/login_resource.py
+++ resources/login_resource.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource
 
-from common import errors
+from common.exception import HttpException
+from common import BaseResource
+
 from models.user import User
 from services import jwt_util
 from .register_resource import base_parser
@@ -9,27 +11,19 @@ login_parser = base_parser.copy()
 login_parser.remove_argument('username')
 
 
-class LoginResource(Resource):
+class LoginResource(BaseResource):
     def post(self):
-        try:
-            args = login_parser.parse_args()
-            user = User.query.filter_by(email=args.email).first()
-            if user is None:
-                return errors['UserNotFoundError']
-            if user.check_password(args.password):
-                auth_token = jwt_util.encode_auth_token(user.id)
-                if auth_token:
-                    response = {
-                        'status': '200',
-                        'token': auth_token
-                    }
-                    return response
-            else:
-                return errors['WrongCredentialError']
-        except Exception as e:
-            print(e)
-            response = {
-                'status': '500',
-                'message': 'Try again'
-            }
-            return response, 500
+        args = login_parser.parse_args()
+        user = User.query.filter_by(email=args.email).first()
+        if user is None:
+            return HttpException(code="003", message="User not found", http_status=404)
+        if user.check_password(args.password):
+            auth_token = jwt_util.encode_auth_token(user.id)
+            if auth_token:
+                response = {
+                    'status': '200',
+                    'token': auth_token
+                }
+                return response
+        else:
+            return HttpException(code="005", message="Wrong credentials error", http_status=400)
