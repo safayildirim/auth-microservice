71dc48ce9728af3474448743ef7355e0b0e12b4a
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-05-27 22:08:43 +0300

    feat(validation): example request validation logic created

--- common/validation.py
+++ common/validation.py
@@ -1,9 +1,103 @@
+from typing import Any, Callable, Mapping
+
+
+def str_is_none(is_none, val: str):
+    if is_none:
+        return val is None
+    return val is not None
+
+def min_len(min_len, val):
+    print("length: %d, value: %s" % (min_len, val))
+    # return {"code": 0, "message": "min length error"}
+    return None
+
+def max_len(max_len, val):
+    print("length: %d, value: %s" % (max_len, val))
+    # return {"code": 0, "message": "max length error"}
+    return None
+
+_constraint_container: Mapping[str, Callable[[Any], None or Exception]] = {
+    'str-is-none': str_is_none,
+    'min_len': min_len,
+    'max_len': max_len,
+}
+
+class Validator:
+    def __init__(self, _type, parameters: Mapping[str, Any]) -> None:
+        self.type = _type
+        self.parameters = parameters
+        self.is_report_ready = False
+        self.general_validation_report = []
+
+    def validate(self, field_value: Any):
+        self.is_report_ready = True
+        for key, constraint_value in self.parameters.items():
+            if key in _constraint_container:
+                validator = _constraint_container[key]
+                validation_response = validator(constraint_value, field_value)
+                self.general_validation_report.append(validation_response)
+    
+    def is_valid(self):
+        if not self.is_report_ready:
+            raise Exception("report is not ready yet, first run validate")
+
+        for report in self.general_validation_report:
+            if report is not None:
+                return False
+        return True
+
+    def raise_error_stack(self):
+        raise Exception(self.general_validation_report)
+
+    def apply(self, value):
+        """
+        First it will run the validate method, if it is not ran yet 
+        If there is an error in the report it will raise whole errors
+        otherwise it will return the incoming value.
+        """
+        if not self.is_report_ready:
+            self.validate(value)
+
+        if not self.is_valid():
+            self.raise_error_stack()
+
+        return value
+        
+
+class Field:
+    @classmethod
+    def __get_clean_params(self, params):
+        return {key: val for key, val in params.items() if val is not None and key != 'self'}
+
+    @classmethod
+    def String(self, is_none=None, is_blank=None, is_empty=None, is_email=None, pattern=None, min_len=None, max_len=None):
+        return Validator('str', self.__get_clean_params(locals()))
+
+    @classmethod
+    def Integer(self, min=None, max=None):
+        return Validator('int', self.__get_clean_params(locals()))
+
+    @classmethod
+    def Float(self, min=None, max=None):
+        return Validator('float', self.__get_clean_params(locals()))
+
+    @classmethod
+    def Object(self):
+        return Validator('object', self.__get_clean_params(locals()))
+
+    @classmethod
+    def Custom(self, name, **kwargs):
+        return Validator('custom', self.__get_clean_params(locals()))
+
+
 class RegisterRequestDTO:
-    email: str = 'test@'
-    password: str = 'test'
+    email = Field.String(is_blank= False, is_email= True)
+    password = Field.String(is_blank= False, min_len= 8, max_len=16)
+    username = Field.String(min_len=2, max_len=25)
+    obj = Field.Custom("custom", email="valid", cool=True)
 
     def __str__(self) -> str:
-        return f'email: {self.email}, password: {self.password}'
+        return "{\n\temail: %s, \n\tusername: %s, \n\tpassword: %s, \n\tobj: %s\n}" % (self.email, self.username, self.password, self.obj)
 
 
 def request(dataType):
@@ -25,9 +119,15 @@ def request(dataType):
     return decorator
 
 
-@request(RegisterRequestDTO)
-def sum(request):
-    print('string request: ', request)
+# @request(RegisterRequestDTO)
+# def sum(request):
+#     print('string request: ', request)
 
+# print(sum())
 
-print(sum())
+r = RegisterRequestDTO()
+# print(r.__dict__)
+# print(r.__dir__())
+
+r.__setattr__('username', r.__getattribute__('username').apply('yuko'))
+print(r)
